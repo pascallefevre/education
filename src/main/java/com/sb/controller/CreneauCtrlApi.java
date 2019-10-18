2cc85f379360ab65017887bbe5597cbb8ed0b17f
Pascal_Lefevre <pascal.lefevre.frde@gmail.com>
2019-10-18 10:34:04 +0200

    second commit project ready for exportation

--- src/main/java/com/sb/controller/CreneauCtrlApi.java
+++ src/main/java/com/sb/controller/CreneauCtrlApi.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.sb.dao.ICreneau;
 import com.sb.domain.Creneau;
 import com.sb.domain.View;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/creneaux")
 public class CreneauCtrlApi {
 
 	@Autowired
--- src/main/java/com/sb/controller/ProfCtrlApi.java
+++ src/main/java/com/sb/controller/ProfCtrlApi.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.sb.dao.IProf;
 import com.sb.domain.Prof;
 import com.sb.domain.View;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/profs")
 public class ProfCtrlApi {
 
 	@Autowired
